7a8664f90fc0005a93beac3c893497f07d3427e9
Jefferson Oliveira <jefferson.tecmec@hotmail.com>
2023-05-31 15:57:47 -0300

    Desenvolvida aplicação e testada ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  
+})
+
+export class FirtComponentComponent { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Todo-List';
+  
+  itens: any[] = []
+  tarefa: any = ""
+  constructor(private httpClient: HttpClient) {
+
+  }
+  ngOnInit(): void {
+    this.mostraDados()
+  
+  }
+
+  mostraDados(){
+    this.httpClient.get("http://localhost:3000/itens").subscribe({
+      next: (snapshot: any) => {
+        console.log(snapshot);
+        this.itens = snapshot;
+      },
+      error: (error) => {
+        alert("Deu ruim")
+      }
+    })
+  }
+
+  adicionaTarefa() {
+    const model = {
+      name: this.tarefa
+    }
+    this.httpClient.post("http://localhost:3000/itens", model).subscribe({
+      next: () => {
+        this.tarefa = "";
+        this.mostraDados();
+      },
+      error: (error) => {
+        alert("Deu ruim")
+      }
+    })
+  }
+  removerTarefa(item: any) {
+    console.log(item)
+    this.httpClient.delete("http://localhost:3000/itens/" + item.id).subscribe({
+      next: () => {
+        this.mostraDados();
+      },
+      error: (error) => {
+        alert("Deu ruim")
+      }
+    })
+
+  }
+  editartarefa(item: any) {
+    
+    item.name = window.prompt("Digite a nova tarefa")
+    this.httpClient.put("http://localhost:3000/itens/" + item.id,item).subscribe({
+      next: () => {
+        this.mostraDados();
+      },
+      error: (error) => {
+        alert("Deu ruim")
+      }
+    })
+  }
 }
